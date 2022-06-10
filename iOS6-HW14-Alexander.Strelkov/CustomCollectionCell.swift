d96a04bd4d7285f554a8d3ce6aa28691464a2e75
Alex <wild6158@gmail.com>
2022-06-10 12:52:01 +0300

    Created methods for making custom cell.

---
+++ iOS6-HW14-Alexander.Strelkov/CustomCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionCell.swift
+//  iOS6-HW14-Alexander.Strelkov
+//
+//  Created by Alexandr Strelkov on 09.06.2022.
+//
+
+import UIKit
+
+class CustomCollectionCell: UICollectionViewCell {
+    static let identifier = "CustomCollectionCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- iOS6-HW14-Alexander.Strelkov/Third/ThirdViewController.swift
+++ iOS6-HW14-Alexander.Strelkov/Third/ThirdViewController.swift
@@ -7,39 +7,42 @@
 
 import UIKit
 
+struct Section {
+    let options: [CollectionViewType]
+}
+enum CollectionViewType {
+    case imageCell(model: CustomData)
+    
+}
 struct CustomData {
-    var image: UIImage
+    var image: UIImage?
 }
 
 class ThirdViewController: UIViewController {
     
-    let data = [
-        CustomData(image: UIImage(named: "image0")!),
-        CustomData(image: UIImage(named: "image1")!),
-        CustomData(image: UIImage(named: "image2")!),
-        CustomData(image: UIImage(named: "image3")!),
-        CustomData(image: UIImage(named: "image4")!),
-        CustomData(image: UIImage(named: "image5")!),
-        CustomData(image: UIImage(named: "image6")!),
-        CustomData(image: UIImage(named: "image7")!)
-    ]
+    var data = [Section]()
     
     //MARK: - Views
-    
-//    private var tableView: UITableView = {
-//        let table = UITableView(frame: .zero, style: .insetGrouped)
-//        table.backgroundColor = .gray
-//        return table
-//    }()
     
     private var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-       
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collection.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        
+        collection.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
         return collection
+    }()
+    
+    private var albumLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        
+        
+        label.text = "Альбомы"
+        
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 35)
+        return label
     }()
     
     //MARK: - Lifecycle
@@ -47,6 +50,7 @@ class ThirdViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configCells()
         collectionView.backgroundColor = .white
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -56,7 +60,6 @@ class ThirdViewController: UIViewController {
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nil, bundle: nil)
-        
         setupHierarchy()
         setupLayout()
     }
@@ -64,32 +67,49 @@ class ThirdViewController: UIViewController {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-
+    
     
     //MARK: - Settings
     
     private func setupHierarchy() {
-//        view.addSubview(tableView)
+        view.addSubview(albumLabel)
         view.addSubview(collectionView)
         
+        
     }
-   
+    
     private func setupLayout() {
-//        tableView.translatesAutoresizingMaskIntoConstraints = false
-//        tableView.frame = view.bounds
-       
+        
+        albumLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            albumLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            albumLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
+        ])
+        
+        
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
-        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, constant: 40).isActive = true
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: albumLabel.bottomAnchor, constant: 5),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, constant: 40)
+        ])
     }
     
     //MARK: - Functions
     
-    
-    
-    
+    func configCells() {
+        data.append(Section(options: [
+            .imageCell(model: CustomData(image: UIImage(named: "image0"))),
+            .imageCell(model: CustomData(image: UIImage(named: "image1"))),
+            .imageCell(model: CustomData(image: UIImage(named: "image2"))),
+            .imageCell(model: CustomData(image: UIImage(named: "image3"))),
+            .imageCell(model: CustomData(image: UIImage(named: "image4"))),
+            .imageCell(model: CustomData(image: UIImage(named: "image5"))),
+            .imageCell(model: CustomData(image: UIImage(named: "image6"))),
+            .imageCell(model: CustomData(image: UIImage(named: "image7"))),
+        ]))
+    }
 }
 
 extension ThirdViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
@@ -98,14 +118,22 @@ extension ThirdViewController: UICollectionViewDelegateFlowLayout, UICollectionV
         return CGSize(width: collectionView.frame.width / 2.5, height: collectionView.frame.width / 2)
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return data.count
+        return data[section].options.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
-        cell.data = self.data[indexPath.row]
         
-        return cell
+        let model = data[indexPath.section].options[indexPath.row]
+        
+        switch model.self {
+        case .imageCell(let model):
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: CustomCollectionViewCell.identifier,
+                for: indexPath) as? CustomCollectionViewCell else {
+                    return UICollectionViewCell()
+                }
+            cell.configureCell(with: model)
+            return cell
+        }
     }
-    
 }
